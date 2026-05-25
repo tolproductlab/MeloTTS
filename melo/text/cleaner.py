ec5747f13a4bc4b94037b88f5f8b5e1be3896e49
tolcodedme <tolcodedme@gmail.com>
2026-05-26 01:26:12 +0200

    fix: lazy imports for JP/KR/ZH modules in cleaner.py - FR only required

--- melo/text/cleaner.py
+++ melo/text/cleaner.py
@@ -1,6 +1,23 @@
-from . import chinese, japanese, english, chinese_mix, korean, french, spanish
+from . import french, english, spanish
 from . import cleaned_text_to_sequence
 import copy
+
+try:
+    from . import chinese
+except ImportError:
+    chinese = None
+try:
+    from . import japanese
+except ImportError:
+    japanese = None
+try:
+    from . import chinese_mix
+except ImportError:
+    chinese_mix = None
+try:
+    from . import korean
+except ImportError:
+    korean = None
 
 language_module_map = {"ZH": chinese, "JP": japanese, "EN": english, 'ZH_MIX_EN': chinese_mix, 'KR': korean,
                     'FR': french, 'SP': spanish, 'ES': spanish}
@@ -8,6 +25,8 @@ language_module_map = {"ZH": chinese, "JP": japanese, "EN": english, 'ZH_MIX_EN'
 
 def clean_text(text, language):
     language_module = language_module_map[language]
+    if language_module is None:
+        raise ImportError(f"Language {language} is not supported because its dependencies are missing.")
     norm_text = language_module.text_normalize(text)
     phones, tones, word2ph = language_module.g2p(norm_text)
     return norm_text, phones, tones, word2ph
@@ -15,6 +34,8 @@ def clean_text(text, language):
 
 def clean_text_bert(text, language, device=None):
     language_module = language_module_map[language]
+    if language_module is None:
+        raise ImportError(f"Language {language} is not supported because its dependencies are missing.")
     norm_text = language_module.text_normalize(text)
     phones, tones, word2ph = language_module.g2p(norm_text)
     
