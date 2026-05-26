963d6dc38870c8afc9a8fbc3c933827f8aa6a404
tolcodedme <tolcodedme@gmail.com>
2026-05-26 03:01:40 +0200

    fix: FR/EN/ES imports obligatoires, lazy import JP/KR/ZH uniquement

--- melo/text/french.py
+++ melo/text/french.py
@@ -4,7 +4,7 @@ import re
 
 from . import symbols
 from .fr_phonemizer import cleaner as fr_cleaner
-from .fr_phonemizer import fr_to_ipa
+from phonemizer import phonemize
 from transformers import AutoTokenizer
 
 
@@ -26,8 +26,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 def g2p(text, pad_start_end=True, tokenized=None):
     if tokenized is None:
         tokenized = tokenizer.tokenize(text)
-    # import pdb; pdb.set_trace()
-    phs = []
+    
     ph_groups = []
     for t in tokenized:
         if not t.startswith("#"):
@@ -38,7 +37,7 @@ def g2p(text, pad_start_end=True, tokenized=None):
     phones = []
     tones = []
     word2ph = []
-    # print(ph_groups)
+    
     for group in ph_groups:
         w = "".join(group)
         phone_len = 0
@@ -46,7 +45,10 @@ def g2p(text, pad_start_end=True, tokenized=None):
         if w == '[UNK]':
             phone_list = ['UNK']
         else:
-            phone_list = list(filter(lambda p: p != " ", fr_to_ipa.fr2ipa(w)))
+            # Use phonemizer with espeak backend for French
+            # language 'fr-fr' for French, strip trailing whitespace
+            phonemes = phonemize(w, language='fr-fr', backend='espeak', strip=True, preserve_punctuation=True)
+            phone_list = list(filter(lambda p: p != " ", phonemes))
         
         for ph in phone_list:
             phones.append(ph)
@@ -54,8 +56,6 @@ def g2p(text, pad_start_end=True, tokenized=None):
             phone_len += 1
         aaa = distribute_phone(phone_len, word_len)
         word2ph += aaa
-        # print(phone_list, aaa)
-        # print('=' * 10)
 
     if pad_start_end:
         phones = ["_"] + phones + ["_"]
@@ -69,26 +69,7 @@ def get_bert_feature(text, word2ph, device=None):
 
 if __name__ == "__main__":
     ori_text = 'Ce service gratuit est“”"" 【disponible》 en chinois 【simplifié] et autres 123'
-    # ori_text = "Ils essayaient vainement de faire comprendre à ma mère qu'avec les cent mille francs que m'avait laissé mon père,"
-    # print(ori_text)
     text = text_normalize(ori_text)
-    print(text)
-    phoneme = fr_to_ipa.fr2ipa(text)
-    print(phoneme)
-
-    
-    from TTS.tts.utils.text.phonemizers.multi_phonemizer import MultiPhonemizer
-    from text.cleaner_multiling import unicleaners
-
-    def text_normalize(text):
-        text = unicleaners(text, cased=True, lang='fr')
-        return text
-
-    # print(ori_text)
-    text = text_normalize(ori_text)
-    print(text)
-    phonemizer = MultiPhonemizer({"fr-fr": "espeak"})
-    # phonemizer.lang_to_phonemizer['fr'].keep_stress = True
-    # phonemizer.lang_to_phonemizer['fr'].use_espeak_phonemes = True
-    phoneme = phonemizer.phonemize(text, separator="", language='fr-fr')
-    print(phoneme)
+    print(f"Normalized: {text}")
+    phones, tones, word2ph = g2p(text)
+    print(f"Phones: {phones}")
